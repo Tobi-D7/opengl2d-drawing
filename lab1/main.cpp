4c97ccd3f101af7f93b89d89ad50922c2d2c6d05
Tobi <tobid7@outlook.de>
2021-09-02 17:58:33 +0200

    Update main.cpp

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -11,6 +11,7 @@ int main(int argc, char **argv){
 	init();
         while (aptMainLoop())
         {
+             pglSelectScreen(GFX_TOP, GFX_LEFT);
              display();
              reshape();
         }
